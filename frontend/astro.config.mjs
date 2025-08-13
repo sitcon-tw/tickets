a9bf48a1cd674e755e7a78001bddd3e0c27e4217
Elvis Mao <info@elvismao.com>
2025-08-13 11:03:10 +0800

    fix: auto detect environment

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
+import cloudflare from "@astrojs/cloudflare";
+import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 import { filterSitemapByDefaultLocale, i18n } from "astro-i18n-aut/integration";
 import { defineConfig } from "astro/config";
 import { defaultLocale, locales } from "./src/i18n/config.js";
+const isCFWorker = process.env.DEPLOY_ENV === "cloudflare";
 
 export default defineConfig({
 	site: "https://example.com",
@@ -10,6 +13,8 @@ export default defineConfig({
 	build: {
 		format: "directory"
 	},
+	adapter: isCFWorker ? cloudflare() : node({ mode: "standalone" }),
+	output: "server",
 	redirects: {
 		// "/zh": {
 		// 	status: 301,
