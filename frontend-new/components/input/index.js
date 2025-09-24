ccd332f91f971cb6142f3da49505d76b90c6c2f2
nelsonGX <hi@nelsongx.com>
2025-09-24 11:01:07 +0800

    feat: copy old files

---
+++ frontend-new/components/input/index.js
@@ -0,0 +1,8 @@
+export { default as Checkbox } from "./Checkbox.astro";
+export { default as Description } from "./Description.astro";
+export { default as Email } from "./Email.astro";
+export { default as MultiSelect } from "./MultiSelect.astro";
+export { default as Radio } from "./Radio.astro";
+export { default as Select } from "./Select.astro";
+export { default as Text } from "./Text.astro";
+export { default as Textarea } from "./Textarea.astro";
---
+++ frontend-new/i18n/index.ts
@@ -0,0 +1,24 @@
+import { defaultLocale, locales } from "./config";
+
+export const t = (lang: string, data: Record<string, Record<string, string>>): Record<string, any> => {
+	const result: Record<string, any> = {};
+	for (const key in data) {
+		result[key] = data[key][lang] || data[key][defaultLocale];
+	}
+	result.t = Object.keys(data[Object.keys(data || {})[0]]);
+	return result;
+};
+
+export const localesList = (): string[] => Object.keys(locales);
+export const local = (url: URL): string => {
+	return localesList().find(lang => url.toString().startsWith(`/${lang}`)) || defaultLocale;
+};
+
+export const l = (url: URL) => {
+	const currentLocale = local(url);
+	return (path?: string, lang: string = ""): string => {
+		const base = lang || currentLocale;
+		if (!path) path = url.pathname.replace(`/${currentLocale}/`, "");
+		return `/${base}${path}`;
+	};
+};
