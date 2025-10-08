da6826ebcb5103d9f0a9eb0f9602f67d3e18f414
nelsonGX <hi@nelsongx.com>
2025-10-08 17:06:20 +0800

    fix: rqeuire auth

--- backend/routes/public.js
+++ backend/routes/public.js
@@ -4,11 +4,12 @@ import invitationCodesRoutes from "./public/invitationCodes.js";
 import referralRoutes from "./public/referrals.js";
 import registrationsRoutes from "./public/registrations.js";
 import promoteRoutes from "./public/promote.js";
+import { requireAuth } from "#middleware/auth.js";
 
 export default async function publicRoutes(fastify) {
 	await fastify.register(eventsRoutes);
 	await fastify.register(ticketsRoutes);
-	await fastify.register(registrationsRoutes);
+	await fastify.register(registrationsRoutes, { preHandler: requireAuth });
 	await fastify.register(referralRoutes);
 	await fastify.register(invitationCodesRoutes);
 	await fastify.register(promoteRoutes);
