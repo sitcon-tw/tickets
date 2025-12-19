52c0bee018b4a402709cda490a674f528415b799
nelsonGX <hi@nelsongx.com>
2025-12-20 05:32:24 +0800

    remove unused font

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -33,8 +33,6 @@ export default function RootLayout({
 				<meta charSet="UTF-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<meta name="generator" content="Next.js" />
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link href="https://font.emtech.cc/css/GenKiGothicTW" rel="stylesheet" />
 			</head>
 
 			<body suppressHydrationWarning>
