c9a8f11fec70d42c7899de13f181c8414fa0345f
nelsonGX <hi@nelsongx.com>
2026-01-24 04:01:06 +0800

    code format

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -1,12 +1,12 @@
-import type { NextRequest } from 'next/server'
 import createMiddleware from "next-intl/middleware";
+import type { NextRequest } from "next/server";
 import { routing } from "./i18n/routing";
 
 const middleware = createMiddleware(routing);
 
 export default function handler(request: NextRequest) {
 	const response = middleware(request);
-	response.headers.set('Vary', 'RSC');
+	response.headers.set("Vary", "RSC");
 	return response;
 }
 
